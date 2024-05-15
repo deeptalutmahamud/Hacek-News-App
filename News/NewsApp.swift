bc0295f7005ed1a8076e060b5a1b4797e328caa8
deeptalutmahamud <mddeep22@gmail.com>
2024-05-16 01:28:53 +0600

    Initial Commit

---
+++ News/NewsApp.swift
@@ -0,0 +1,17 @@
+//
+//  NewsApp.swift
+//  News
+//
+//  Created by Talut mahamud Deep on 16/5/24.
+//
+
+import SwiftUI
+
+@main
+struct NewsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
