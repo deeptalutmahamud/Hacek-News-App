61e60b7765c231d750e8af4cd1ea071007176d67
deeptalutmahamud <mddeep22@gmail.com>
2024-05-21 00:40:35 +0600

    nothing

---
+++ News/View/Loader.swift
@@ -0,0 +1,18 @@
+//
+//  Loader.swift
+//  News
+//
+//  Created by Talut mahamud Deep on 21/5/24.
+//
+
+import SwiftUI
+
+struct Loader: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Loader()
+}
