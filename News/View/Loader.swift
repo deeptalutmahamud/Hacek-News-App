fcbb0979b71f61e77bc7a18fb712c428172cfe3c
deeptalutmahamud <mddeep22@gmail.com>
2024-05-21 00:44:37 +0600

    video

--- News/View/Loader.swift
+++ News/View/Loader.swift
@@ -6,13 +6,51 @@
 //
 
 import SwiftUI
+import SSSwiftUIGIFView
 
-struct Loader: View {
+struct LoaderView<Content>: View where Content: View {
+    
+    @Binding var isShowing: Bool
+    var content: () -> Content
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        GeometryReader { geometry in
+           
+                ZStack(alignment: .center) {
+                    self.content()
+                        .disabled(self.isShowing)
+                        
+                       
+                    ZStack {
+                        SwiftUIGIFPlayerView(gifName: "loading") .frame(width: 200,height: 200)
+                        //UrbanistMediumTextView(text: "", fontSize: 15, colorName: "191B32")
+                    }.opacity(self.isShowing ? 1 : 0)
+                        .padding(15)
+                        .background(self.isShowing ? Color.white.opacity(0) : Color.clear)
+                        .cornerRadius(65)
+                    
+                    
+                }
+                //.padding(.top, -40)
+            
+//            .frame(width: UIScreen.main.bounds.width, height: UIScreen.main.bounds.height)
+//            .background(Color.black.opacity(0.3))
+//            .ignoresSafeArea()
+        }
+        
     }
+        
 }
 
+
+
+
+
 #Preview {
-    Loader()
+    LoaderView(isShowing: .constant(true)){
+        VStack{
+            Text("")
+        }
+        .frame(width: UIScreen.main.bounds.width, height: UIScreen.main.bounds.height, alignment: .center)
+    }
 }
